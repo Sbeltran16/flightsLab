7ef1b93564083213e6e8e89920c57b18f6944c50
Sbeltran16 <sbeltran16@gmail.com>
2022-04-05 02:09:13 -0700

    updated show file

--- controllers/flights.js
+++ controllers/flights.js
@@ -3,6 +3,9 @@ const Flight = require('../models/flight')
 
 function show(req, res){
     Flight.findById(req.params.id, function(err, flight){
+        Ticket.find({flight: flight._id}, function(err, tickets) {
+            res.render("flights/show", {title: "All flights", flight: flight});
+        });
         res.render("flights/show", {title: "All flights", flight: flight});
     })
 }
